ec64e1ca0c41f2671aaf3810063872d770498bf1
qyj <1078869122@qq.com>
2022-07-02 19:33:56 +0800

    ignore /dist

--- dist/components/theme/Theme.d.ts
+++
@@ -1,9 +0,0 @@
-import React, { FC, ReactNode } from "react";
-export declare type theme = 'dark' | 'light';
-interface ThemeProps {
-    children: ReactNode;
-    theme?: theme;
-}
-export declare const ThemeContext: React.Context<string>;
-declare const ThemeProvider: FC<ThemeProps>;
-export default ThemeProvider;
--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { default as Button } from "./components/button/Button";
-export { default as ThemeProvider } from "./components/theme/Theme";
