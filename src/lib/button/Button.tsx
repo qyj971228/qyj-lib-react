60e725c420589eeac756aa9df37390b3d8bcdff8
qyj <1078869122@qq.com>
2023-03-31 21:31:04 +0800

    refactor: remove react import

--- src/lib/button/Button.tsx
+++ src/lib/button/Button.tsx
@@ -1,5 +1,4 @@
 import './style.css'
-import React from 'react'
 
 export default function Button({ props, children, ...rest }: any) {
   return <button className='button' {...rest}>{children}</button>
--- src/lib/input/Input.tsx
+++ src/lib/input/Input.tsx
@@ -1,5 +1,4 @@
 import './style.css'
-import React from 'react'
 
 export default function Input({ props, ...rest }: any) {
   return <input className='input' {...rest} />
